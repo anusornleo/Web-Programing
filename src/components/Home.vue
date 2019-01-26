307ad2b5af6c16ce0516e5d4220f9a9d0654b485
Anusorn Mennakred <anusornleo@git.com>
2019-01-26 19:24:56 +0700

    First import Project Movie via Vue.js in Web Programimg

---
+++ src/components/Home.vue
@@ -0,0 +1,106 @@
+<!-- src/components/Home.vue -->
+<template>
+  <div class="bg_login">
+    <div class="harf_bg">
+      <h1>Sign in</h1>
+      <form action="main.vue" class="center_login">
+        <input type="email" id="email" name="email" placeholder="Email">
+        <input type="password" id="password" name="password" placeholder="Password">
+        <input type="submit" value="Login" @click="gomain">
+      </form>
+      <button type="button" class="btn btn-default btn-lg" @click="goAdd">
+      <span class="glyphicon glyphicon-plus" aria-hidden="true" ></span> Add
+    </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  methods: {
+    goAdd: function() {
+      this.$router.push("/main");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.bg_login{
+  background-color: #00897b;
+  width: 100%;
+  height: -webkit-fill-available;
+}
+.harf_bg {
+  background-color: white;
+  position: absolute;
+  top: 0px;
+  left: 50%;
+  width: 50%;
+  height: -webkit-fill-available;
+}
+
+/* input username or password CSS */
+
+h1 {
+  text-align: center;
+  padding: 12% 0px 0px 3%;
+}
+
+input[type="email"] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border: 3px solid #ccc;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+  outline: none;
+  border-radius: 24px;
+}
+
+input[type="password"] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  border: 3px solid #ccc;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+  outline: none;
+  border-radius: 24px;
+}
+
+input[type="submit"] {
+  width: 30%;
+  background-color: #b2dfdb;
+  color: rgb(255, 255, 255);
+  padding: 13px 20px;
+  margin: 8px 35%;
+  border: none;
+  border-radius: 24px;
+  cursor: pointer;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+}
+
+input[type="email"]:focus {
+  border: 3px solid #555;
+}
+
+input[type="password"]:focus {
+  border: 3px solid #555;
+}
+
+input[type="submit"]:hover {
+  background-color: #00897b;
+  color: rgb(255, 255, 255);
+}
+
+.center_login {
+  position: absolute;
+  margin: 4% 0 0 27%;
+  width: 50%;
+}
+</style>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div class="grid-container">
+      <div class="grid-item">
+        <img class="poster" src="poster_test.jpg">
+        <div class="overlay">
+          <div class="text">Avenger : End Game</div>
+        </div>
+      </div>
+      <div class="grid-item">
+        <img class="poster" src="poster_test.jpg">
+        <div class="overlay">
+          <div class="text">Hello World</div>
+        </div>
+      </div>
+      <div class="grid-item">
+        <img class="poster" src="poster_test.jpg">
+        <div class="overlay">
+          <div class="text">Hello World</div>
+        </div>
+      </div>
+      <div class="grid-item">
+        <img class="poster" src="poster_test.jpg">
+        <div class="overlay">
+          <div class="text">Hello World</div>
+        </div>
+      </div>
+      <div class="grid-item">
+        <img class="poster" src="poster_test.jpg">
+        <div class="overlay">
+          <div class="text">Hello World</div>
+        </div>
+      </div>
+      <div class="grid-item">
+        <img class="poster" src="poster_test.jpg">
+        <div class="overlay">
+          <div class="text">Hello World</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+/* home card movie */
+
+.grid-container {
+  width: 1000px;
+  display: grid;
+  margin: auto;
+  margin-top: 100px;
+  grid-template-columns: auto auto auto auto;
+}
+
+.grid-item {
+  position: relative;
+  background-color: rgba(255, 255, 255, 0.8);
+  margin: 0 0px 32px 0;
+  width: 225px;
+  height: 350px;
+  padding: 0px;
+  font-size: 30px;
+  text-align: center;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.overlay {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgb(255, 255, 255);
+  overflow: hidden;
+  width: 100%;
+  height: 20%;
+  transition: 0.5s ease;
+}
+
+.grid-item:hover .overlay {
+  height: 100%;
+}
+.poster {
+  width: 100%;
+  height: 80%;
+}
+.text {
+  font-size: 22px;
+  padding: 2px;
+}
+</style>
+
+
